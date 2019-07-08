07b37f28ca06f6874afc5c8f5f8644388cbe87ae
meowxiik <meowxiik@gmail.com>
2019-07-08 19:04:59 +0200

    Added many features

--- README.md
+++ README.md
@@ -1,29 +1,115 @@
 # Environment Overseer
 
-This small Python script allows one to control their environment.
-It uses user-defined enable and disable scripts and internal timer to run these scripts.
+This small Python script allows one to execute other scripts based on environment conditions.
 
-## Examples
+Goal of this project is to limit my own procrastination, using scripts to detect if one is for example watching 
+YouTube and record this, possibly block if YouTube's limit was reached.
 
-* [X] Limit internet to x hours a day
-* [X] Limit Steam to y hours a day
-* [X] Limit YouTube to z hours a week
+Overseer is designed to run on a upstream network device (router) to be able to intercept and block traffic. (Tested with PiHole)
 
+## Capabilities
+* [X] Manual enabling / disabling of activities
+* [X] Keeping track of activity enabled time
+* [X] Automatic enabling / disabling activities based on time of day
+* [X] Checking whether system is conformant to enable / disable scripts, executing them again if not
 
 ## Usage
 
 `overseer`: Start main daemon of the app  
 `overseer -e <activity>`: Enables an activity  
 `overseer -d <activity>`: Disables an activity  
-`overseer -r`: Resets the time intervals for all activities  
-`overseer -w`: Copies folder `today` to dedicated `history` folder, useful for keeping a history
+`overseer -r`: Resets timers for all activities  
+`overseer -l`: Prints currently active activities (Effectively `ls /etc/overseer/status`)  
+`overseer -p`: Prepares Overseer's file structure
 
-In examples above, `<activity>` is always a name of an activity.
+## JSON Activity Definition
 
-`overseer`, `overseer -r` nor `overseer -w` will not be ran automatically, yet they are designed to be.
-Thus it is advisable to run these automatically, either using systemd or cron.
-All commands need a prior `overseer` to be running. There can only be one `overseer` running at one time, preferably all the time.
+Activity definition must be a `.json` file with `json` structure in folder `/etc/overseer/activities/`.
+Overseer supports following properties:
 
+`Limit`: When activity reaches limit set by this property, it is automatically disabled and it's enabling is blocked. Format must match following `integer{H,h,S,s,M,m}`
+
+`AutoStart`: Activity will be automatically enabled at this time. Time in format `%H:%M`.
+
+`AutoStop`: Activity will be automatically disabled at this time. Same format as `AutoStart`
+
+Note that `AutoStart` and `AutoStop` do not force keep the activity enabled or disabled.
+It's the equivalent of executing `overseer -e` at `AutoStart` or `overseer -d` at `AutoStop`
+
+###Examples
+```
+{
+    "Limit": "30M",
+}
+```
+
+```
+{
+    "Limit": "3H", 
+    "AutoStart":"14:00",
+    "AutoStop":"16:00"
+}
+``` 
+
+## Example configuration
+
+Requisites:
+* Linux device with it's own AP (ArchLinux preferably, but not necessarily)
+* PiHole on this device
+* Overseer
+
+Description:
+ * Let's setup blocking of YouTube, allowing it only for 30 minutes a day
+
+WiFi & PiHole setup:
+ * Wireless AP on your Linux machine guide [(link)](https://wiki.archlinux.org/index.php/Software_access_point)
+ * PiHole guide [(link)](https://wiki.archlinux.org/index.php/Pi-hole)
+
+Setup Overseer:
+1. Download EnvironmentOverseer
+    * `git clone https://gitlab.com/meowxiik/environment-overseer.git/`
+2. Copy EnvironmentOverseer to executable location
+    * `cd environment-overseer`
+    * `sudo cp overseer /usr/bin/`
+3. Run `overseer -p`
+4. Populate folders in /etc/overseer
+
+##### `/etc/overseer/activities/youtube`
+``` 
+{
+    "Limit": "30M"
+}
+```
+
+##### `/etc/overseer/scripts/exec/youtube`
+```
+#!/usr/bin/bash
+
+pihole -w -nr youtube.com
+pihole -w -nr www.youtube.com
+pihole -w -nr youtubei.googleapis.com
+pihole --wild -d -nr googlevideo.com
+
+pihole restartdns
+```
+
+##### `/etc/overseer/exec/disable/youtube`
+```
+#!/usr/bin/bash
+
+pihole -b -nr youtube.com
+pihole -b -nr www.youtube.com
+pihole -b -nr youtubei.googleapis.com
+pihole --wild -nr googlevideo.com
+
+pihole restartdns
+```
+
+Now you are ready!
+Run `sudo overseer &` to start it in background and if you go to your workstation connected to the AP
+you should not have access to YouTube!
+
+And try `sudo overseer -e youtube`, now if you refresh, YouTube will be enabled!
 
 ## Structure
 
@@ -34,20 +120,28 @@ Last file you need is `limits/<activity>` which should contain the time one is a
 Ergo, the total structure is as following:
 ```
 /etc/environment-overseer/
-├── limits
-│   └── <activity>
-├── scripts
+├── activities
+│   └── <activity>.json
+├── exec
 │   ├── disable
 │   │   └── <activity>
-│   └── enable
+│   ├── enable
+│   │   └── <activity>
+│   └── status # Optional
 │       └── <activity>
-├── today
+├── timers
 │   └── <activity> # Auto generated, do not create, contains number of seconds activity spent enabled
-└── active
-    └── <activity> # Only exists if the activity is currently enabled
+└── status
+    └── <activity>.json # Only exists if the activity is currently enabled
 ```
 
 In examples above `<activity>` is always a single file, named by the activity itself.
 
-Folders `today` and `active` are both runtime generated, you should only read from those.
-They are refreshed every ~60 seconds.
+##Warning
+
+Neither `overseer` or `overseer -r` will be ran automatically, yet they are designed to be, use systemd or similar to run it automatically.
+`overseer` as `simple` systemd unit type, `overseer -r` as `oneshot`.
+
+Note that it is not very hard to get around it (All you have to do is `overseer -r` and you have access to everything again)
+It is designed to make it more difficult to get to "dangerous" sites ergo when one is feeling lazy, it is not worth going trough all the trouble of
+forcibly enabling it.
---
+++ overseer.py
@@ -0,0 +1,409 @@
+#!/usr/bin/python
+
+import argparse
+import itertools
+import os
+import sched
+import signal
+import time
+import psutil
+import json
+import datetime
+import subprocess
+
+
+def die(reason):
+    print(exit_codes[reason][1])
+    exit(exit_codes[reason][0])
+
+
+parser = argparse.ArgumentParser(description="Controls the environment overseer")
+parser.add_argument('-e', '--enable', nargs='+', help='Enables an activity')
+parser.add_argument('-d', '--disable', nargs='+', help='Disables an activity')
+parser.add_argument('-l', '--list', action='store_true', help='List usage activities')
+parser.add_argument('-r', '--reset', action='store_true', help='Reset all timers')
+parser.add_argument('-p', '--reset', action='store_true', help='Prepares file structure')
+
+exit_codes = {
+    "success": (0, "Success"),
+    "daemon_running": (1, "Daemon is running"),
+    "daemon_not_running": (2, "Daemon is not running"),
+    "root": (3, "Must be ran as root"),
+    "misconfiguration": (4, "Overseer is misconfigured")
+}
+
+parser.epilog = "Exit Codes: " + " ".join([f"{exit_codes[pair][0]}:{exit_codes[pair][1]}" for pair in exit_codes])
+
+args = parser.parse_args()
+
+enabled_activities = []
+bumped_at = time.time()
+timer = sched.scheduler(time.time, time.sleep)
+
+path_home = "/etc/overseer"
+
+path_definitions = f"{path_home}/activities"  # Stores activity definitions
+path_status = f"{path_home}/status"  # Stores which activities are currently used, contains symlinks to path_activities
+path_timers = f"{path_home}/timers"  # Stores activity usage times
+
+path_scripts_enable = f"{path_home}/exec/enable"
+path_scripts_disable = f"{path_home}/exec/disable"
+path_scripts_status = f"{path_home}/exec/status"
+
+path_pid = f"/run/overseer.pid"
+
+
+def sigusr(_, __):
+    bump()
+
+
+def sigusr2(_, __):
+    reset_timers()
+
+
+def link_enable(name):
+    if not os.path.islink(f"{path_status}/{name}"):
+        os.symlink(f"{path_definitions}/{name}", f"{path_status}/{name}")
+
+
+def link_disable(name):
+    if os.path.islink(f"{path_status}/{name}"):
+        os.remove(f"{path_status}/{name}")
+
+
+def reset_timers():
+    for name in os.listdir(f"{path_timers}"):
+        os.remove(f"{path_timers}/{name}")
+
+    for name in os.listdir(f"{path_status}"):
+        os.remove(f"{path_status}/{name}")
+        run_disable(name)
+
+    for name in os.listdir(f"{path_definitions}"):
+        update_time(name, 0)
+
+
+def bump(force_run=False):
+    """
+    Searches for newly enabled / disabled activities
+    Searches for activities which ran out of time
+
+    Updates files for usage activities
+
+    :param force_run: Forces bump to run disable or enable scripts of all activities
+    """
+
+    global bumped_at
+
+    # --------------------------------------------
+    # - PRELIMINARY TIMER PREPARATIONS           -
+    # --------------------------------------------
+    for event in timer.queue:  # Remove any interfering bumps
+        timer.cancel(event)
+
+    print("----Bumping----")
+    create_all_records()
+    activities = parse_activities()
+    active = os.listdir(path_status)
+
+    just_enabled = []
+    just_disabled = []
+
+    # --------------------------------------------
+    # - FIND NEWLY ENABLED / DISABLED ACTIVITIES -
+    # --------------------------------------------
+    for activity in active:
+        if not enabled_activities.__contains__(activity):
+            just_enabled.append(activity)
+            enabled_activities.append(activity)
+
+    for activity in enabled_activities:
+        if not active.__contains__(activity):
+            just_disabled.append(activity)
+            enabled_activities.remove(activity)
+
+    # --------------------------------------------
+    # - FIND ACTIVITIES SCHEDULED START / STOP   -
+    # --------------------------------------------
+
+    prev_time = datetime.datetime.fromtimestamp(bumped_at).time()
+    now_time = datetime.datetime.now().time()
+
+    for activity in activities:
+
+        if not activity.__contains__("AutoStart"):
+            continue
+
+        start_time = datetime.datetime.strptime(activity["AutoStart"], "%H:%M").time()
+
+        if prev_time < start_time <= now_time:
+            if not enabled_activities.__contains__(activity):
+                just_enabled.append(activity)
+
+    for activity in activities:
+
+        if not activity.__contains__("AutoStop"):
+            continue
+
+        stop_time = datetime.datetime.strptime(activity["AutoStop"], "%H:%M").time()
+        if prev_time < stop_time <= now_time:
+            if enabled_activities.__contains__(activity):
+                just_disabled.append(activity)
+
+    # -----------------------------------------------------------------
+    # - CHECK STATUS SCRIPTS -> FIND IF ANY ACTIVS. STOPPED / STARTED -
+    # -----------------------------------------------------------------
+
+    for activity in activities:
+
+        if status_script_exists(activity):
+
+            activity_status = status_script_run(activity)
+
+            if enabled_activities.__contains__(activity):
+                if not activity_status:
+                    just_enabled.append(activity)
+            else:
+                if activity_status:
+                    just_disabled.append(activity)
+
+    # --------------------------------------------
+    # - CALCULATING NEW TIMES                    -
+    # --------------------------------------------
+    time_passed = time.time() - bumped_at
+    for activity in itertools.chain(enabled_activities, just_disabled):
+
+        if just_enabled.__contains__(activity):
+            continue
+
+        activity_time = get_activity_time(activity) + time_passed
+        update_time(activity, activity_time)
+
+    for activity in enabled_activities:
+        if activity["Limit"] != 0 and get_activity_time(activity) > activity["Limit"]:
+            just_disabled.append(activity)
+            enabled_activities.remove(activity)
+
+    bumped_at = time.time()
+
+    # --------------------------------------------
+    # - RUNNING ENABLE / DISABLE SCRIPTS         -
+    # --------------------------------------------
+    for activity in activities:
+        if just_enabled.__contains__(activity) and just_disabled.__contains__(activity):
+            just_enabled.remove(activity)
+            just_disabled.remove(activity)
+        if not just_enabled.__contains__(activity) and not just_disabled.__contains__(activity) and force_run:
+            just_disabled.append(activity)
+
+    for activity in just_enabled:
+        run_enable(activity)
+
+    for activity in just_disabled:
+        run_disable(activity)
+        if os.path.islink(f"usage/{activity}"):
+            os.remove(f"usage/{activity}")
+
+    # --------------------------------------------
+    # - SCHEDULING NEXT BUMP                     -
+    # --------------------------------------------
+    next_bump = 60
+    print(f"Scheduling next bump in {next_bump} seconds")
+    timer.enter(next_bump, 1, bump)
+
+
+def parse_activities():
+    names = os.listdir(path_definitions)
+    activities = []
+    for name in names:
+        with open(f"{path_definitions}/{name}", 'r') as f:
+            activity = json.load(f)
+        activity["name"] = name.split(".")[0]
+        activities.append(activity)
+
+    for activity in activities:
+        limit_raw = activity["Limit"]
+        limit = 0
+
+        try:
+            limit = int(limit_raw[:-1])
+        except ValueError:
+            print(f"Could not parse limit \"{limit_raw}\" of activity {activity['name']}")
+            die("misconfiguration")
+
+        unit = limit_raw[-1:]
+
+        if unit.lower() == 'h':
+            limit = limit * 3600
+        elif unit.lower() == 'm':
+            limit = limit * 60
+        elif unit.lower() == 's':
+            limit = limit
+        else:
+            print(f"Unknown time unit '{unit}' in activity {activity['name']}")
+            die("misconfiguration")
+
+        activity["Limit"] = limit
+
+    return activities
+
+
+def run_enable(activity):
+    activity = activity["name"]
+    print(f"Running enable for {activity}")
+    os.system(f"{path_scripts_enable}/{activity}")
+
+
+def run_disable(activity):
+    activity = activity["name"]
+    print(f"Running disable for {activity}")
+    os.system(f"{path_scripts_disable}/{activity}")
+
+
+def status_script_exists(activity):
+    return os.listdir(path_scripts_status).__contains__(activity["name"])
+
+
+def status_script_run(activity):
+    script = subprocess.run(f"{path_scripts_status}/{activity['name']}", stdout=subprocess.PIPE)
+    return script.returncode != 0
+
+
+def activity_exists(activity):
+    return os.path.isfile(f"{path_definitions}/{activity}")
+
+
+def get_activity_time(activity):
+    path = f"{path_timers}/{activity['name']}"
+    with open(path, 'r') as content_file:
+        time_spent = content_file.read()
+    time_spent = float(time_spent)
+    return time_spent
+
+
+def create_all_records():
+    for activity in os.listdir(path_definitions):
+        activity = activity.split('.')[0]
+        create_record_if_non_existent(activity)
+
+
+def create_record_if_non_existent(activity):
+    path = f"{path_timers}/{activity}"
+
+    if os.path.isfile(path):
+        return
+
+    with open(path, 'w') as content_file:
+        content_file.write('0')
+
+
+def update_time(activity, seconds):
+    with open(f"{path_timers}/{activity}", 'w') as file:
+        file.write(str(int(seconds)))
+
+
+def create_folders_if_non_existent():
+    if not os.path.isdir(path_home):
+        os.makedirs(path_home)
+    if not os.path.isdir(path_definitions):
+        os.makedirs(path_definitions)
+    if not os.path.isdir(path_status):
+        os.makedirs(path_status)
+    if not os.path.isdir(path_timers):
+        os.makedirs(path_timers)
+    if not os.path.isdir(path_scripts_enable):
+        os.makedirs(path_scripts_enable)
+    if not os.path.isdir(path_scripts_disable):
+        os.makedirs(path_scripts_disable)
+    if not os.path.isdir(path_scripts_status):
+        os.makedirs(path_scripts_status)
+
+
+def remote_bump():
+    with open(path_pid, 'r') as content_file:
+        pid = content_file.read()
+        pid = int(pid)
+        os.kill(pid, signal.SIGUSR1)
+
+
+def remote_reset():
+    with open(path_pid, 'r') as content_file:
+        pid = content_file.read()
+        pid = int(pid)
+        os.kill(pid, signal.SIGUSR2)
+
+
+def is_daemon_running():
+    if not os.path.isfile(path_pid):
+        return False
+
+    with open(path_pid, 'r') as content_file:
+        pid = content_file.read()
+        pid = int(pid)
+
+        if psutil.pid_exists(pid):
+            return True
+        else:
+            return False
+
+
+def is_privileged():
+    return os.getuid() == 0
+
+
+if __name__ == "__main__":
+    start_daemon = not args.enable and \
+                   not args.disable and \
+                   not args.reset and \
+                   not args.list
+
+    if start_daemon and is_daemon_running():
+        die("daemon_running")
+
+    if not is_privileged():
+        die("root")
+
+    if not start_daemon and not is_daemon_running():
+        die("daemon_not_running")
+
+    if args.reset:
+        print("Resetting limits...")
+        os.chdir(path_home)
+        remote_reset()
+
+    if args.enable:
+        print("Enabling activities...")
+        os.chdir(path_home)
+        for a in args.enable:
+            if activity_exists(a):
+                link_enable(a)
+            else:
+                print(f"Did not find '{a}' activity")
+        remote_bump()
+
+    if args.disable:
+        print("Disabling activities...")
+        os.chdir(path_home)
+        for a in args.disable:
+            if activity_exists(a):
+                link_disable(a)
+            else:
+                print(f"Did not find '{a}' activity")
+        remote_bump()
+
+    if args.list:
+        pass
+
+    if start_daemon:
+        print("Starting as daemon...")
+        create_folders_if_non_existent()
+        os.chdir(path_home)
+        create_all_records()
+
+        signal.signal(signal.SIGUSR1, sigusr)
+        signal.signal(signal.SIGUSR2, sigusr2)
+        bump(force_run=True)
+        with open(path_pid, "w") as pidf:
+            pidf.write(str(os.getpid()))
+            pidf.close()
+        timer.run()
