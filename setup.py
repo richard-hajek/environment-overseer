81f3c5496ca6b76a21d03cbf90ec7ca9dc7447e2
meowxiik <meowxiik@gmail.com>
2020-09-29 01:55:15 +0200

    Changed packaging package

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 # setup.py
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='overseer',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="overseer-meowxiik",  # Replace with your own username
     version='1.1.0',
-    scripts=['src/overseer.py',],
+    author="meowxiik",
+    author_email="meowxiik@gmail.com",
+    description="An app to control your behavior",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/richard-hajek/environment-overseer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
