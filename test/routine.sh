915348c417855d6c9a8f28440cdba49dd00aa241
meowxiik <meowxiik@gmail.com>
2020-09-30 11:14:55 +0200

    Added interrupt tests

--- test/routine.sh
+++ test/routine.sh
@@ -52,17 +52,23 @@ function final() {
   for t in "${SUCCESSES[@]}"; do printf "--" "-${t}\n" ; done
   echo "Failed: ${#FAILURES[@]}: "
   for t in "${FAILURES[@]}"; do printf "--" "-${t}\n" ; done
+
+  if (( ${#FAILURES[@]} > 0 )) ; then
+    exit 1
+  else
+    exit 0
+  fi
 }
 
 function test_limit {
   settime "00:00:00"
   prepare "limit.json"
   startoverseer
-  overseer -e limit
+  overseer --enable limit
   settime "01:00:00"
-  overseer -t
+  overseer --tick
   announce "limit" "`overseer -l | grep -q disabled; echo $?`"
-  overseer -s
+  overseer --stop
 }
 
 function test_control {
@@ -70,13 +76,34 @@ function test_control {
   prepare "limit.json"
   startoverseer
   announce "initial disable" "`overseer -l | grep -q disabled; echo $?`"
-  overseer -e limit
+  overseer --enable limit
   announce "manual enable" "`overseer -l | grep -q 'enabled!'; echo $?`"
-  overseer -d limit
-  announce "manual disable" "`overseer -l | grep -q 'enabled!'; echo $?`"
-  overseer -s
+  overseer --disable limit
+  announce "manual disable" "`overseer -l | grep -q 'disabled'; echo $?`"
+  overseer --stop
 }
 
-test_limit
-test_control
+function test_interrupt {
+  settime "00:00:00"
+  prepare "inter.json"
+  sleep 1
+  startoverseer
+  overseer --enable inter
+  settime "01:00:00"
+  overseer --tick
+  announce "interrupt" "`overseer -l | grep -q 'interrupt'; echo $?`"
+  settime "02:00:00"
+  sleep 4 # for some reasons theses sleeps are necessary /shrug
+  overseer --tick
+  sleep 4
+  announce "interrupt end" "`overseer -l | grep -q 'disabled'; echo $?`"
+  overseer --stop
+}
+
+# The sleeps everywhere are necessary because they seem to fix issues with bad timing
+
+sleep 2
+test_limit && sleep 3
+test_control && sleep 3
+test_interrupt && sleep 3
 final
